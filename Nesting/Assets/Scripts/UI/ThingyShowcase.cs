dcd71f4ef766550abc2458c36c415c27a2d9fc37
Allen West <allen@awestsecurity.com>
2024-05-31 20:56:44 -0400

    key item display scale bug fixed

--- Nesting/Assets/Scripts/UI/ThingyShowcase.cs
+++ Nesting/Assets/Scripts/UI/ThingyShowcase.cs
@@ -9,10 +9,12 @@ public class ThingyShowcase : MonoBehaviour
 	public float speed = 10;
 	public float timeDisplayed = 5;
 	public GameObject baseObject;
+	public GameObject backdrop;
 	private Transform baseObjectTrans;
 	private MeshFilter baseObjectMesh;
 	private MeshRenderer baseObjectRender;
 	private Vector3 scaleTo;
+	private Vector3 originalScale;
 	
     // Start is called before the first frame update
     void Start()
@@ -22,9 +24,11 @@ public class ThingyShowcase : MonoBehaviour
 		baseObjectMesh = baseObject.GetComponent<MeshFilter>();
 		baseObjectRender = baseObject.GetComponent<MeshRenderer>();
 		baseObjectRender.enabled = false;
+		backdrop.SetActive(false);
 		
-		Bounds tempBounds = baseObjectMesh.sharedMesh.bounds;
+		Bounds tempBounds = baseObjectMesh.mesh.bounds;
 		scaleTo = tempBounds.size;
+		originalScale = baseObjectTrans.localScale;
     }
 
     // Update is called once per frame
@@ -36,8 +40,6 @@ public class ThingyShowcase : MonoBehaviour
 	public void SwitchThing(GameObject newThing) {
 		Mesh newMesh = newThing.GetComponent<MeshFilter>().sharedMesh;
 		baseObjectMesh.sharedMesh = newMesh;
-		//MeshRenderer newRender = newThing.GetComponent<MeshRenderer>();
-		//for (int i = 0; i <)
 		baseObjectRender.materials = newThing.GetComponent<MeshRenderer>().materials;
 		ScaleToBounds();
 		StartCoroutine(Display());
@@ -45,8 +47,10 @@ public class ThingyShowcase : MonoBehaviour
 	
 	private IEnumerator Display() {
 		baseObjectRender.enabled = true;
+		backdrop.SetActive(true);
 		yield return new WaitForSeconds(timeDisplayed);
 		baseObjectRender.enabled = false;
+		backdrop.SetActive(false);
 	}
 	
 	private void ScaleToBounds() {
@@ -55,6 +59,10 @@ public class ThingyShowcase : MonoBehaviour
 		float xdif = scaleTo.x / size.x;
 		float ydif = scaleTo.y / size.y;
 		float zdif = scaleTo.z / size.z;
+		//Debug.Log($"Object size: {size}");
+		//Debug.Log($"Scale to: {scaleTo.x}, {scaleTo.y}, {scaleTo.z}");
+		//Debug.Log($"results: {xdif}, {ydif}, {zdif}");
+
 		float min;
 		if (xdif < ydif) {
 			min = xdif;
@@ -64,6 +72,6 @@ public class ThingyShowcase : MonoBehaviour
 		if (zdif < min) {
 			min = zdif;
 		}
-		baseObjectTrans.localScale = baseObjectTrans.localScale * min;
+		baseObjectTrans.localScale = originalScale * min;
 	}
 }
